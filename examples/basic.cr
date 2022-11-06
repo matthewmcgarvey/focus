59498726b7dc2add46149951c2a2ac0400d045ff
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-11-06 16:16:44 -0600

    Add in remaining binary operators

--- examples/basic.cr
+++ examples/basic.cr
@@ -19,34 +19,34 @@ class Users
   class_getter name : Stealth::Column(String) do
     Stealth::Column.new(table: self, name: "name", sql_type: String)
   end
+  class_getter age : Stealth::Column(Int32) do
+    Stealth::Column.new(table: self, name: "age", sql_type: Int32)
+  end
 end
 
 database.with_connection do |conn|
-  conn.exec("create table if not exists users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(128));")
+  conn.exec("create table if not exists users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(128), age INTEGER);")
 end
 
 database.insert(Users) do
   set(Users.name, "bobby")
+  set(Users.age, 45)
 end
 
 database.insert(Users) do
   set(Users.name, "billy")
-end
-
-database.update(Users) do
-  set(Users.name, "sally")
-  where(Users.name.eq("billy"))
+  set(Users.age, 24)
 end
 
 query = database.from(Users)
-  .select(Users.columns)
-  .where(Users.name.eq("sally"))
+  .select(Users.age)
+  .where(Users.age.not_eq 45)
 
 # puts query.to_sql
 query.each do |row|
   # val = {id: row.get_int32(0)}
-  val = {name: row.get(Users.name), id: row.get(Users.id)}
-  # val = {count: row.get_int32(0)}
+  # val = {name: row.get(Users.name), id: row.get(Users.id)}
+  val = {count: row.get_int32(0)}
   pp val
 end
 
--- src/stealth/dsl/operators.cr
+++ src/stealth/dsl/operators.cr
@@ -1,11 +1,28 @@
 module Stealth::Dsl::Operators(T)
-  def eq(val : T) : Stealth::BinaryExpression(Bool)
+  def eq(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
     Stealth::BinaryExpression.new(
       Stealth::BinaryExpressionType::EQUAL,
       left: as_expression,
-      right: wrap_argument(val),
+      right: expr,
       sql_type: Bool
     )
+  end
+
+  def eq(val : T) : Stealth::BinaryExpression(Bool)
+    eq(wrap_argument(val))
+  end
+
+  def not_eq(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      Stealth::BinaryExpressionType::NOT_EQUAL,
+      left: as_expression,
+      right: expr,
+      sql_type: Bool
+    )
+  end
+
+  def not_eq(val : T) : Stealth::BinaryExpression(Bool)
+    not_eq(wrap_argument(val))
   end
 
   def between(range : Range(T, T)) : BetweenExpression(T)
@@ -50,5 +67,227 @@ module Stealth::Dsl::Operators(T)
       operand: as_expression,
       sql_type: Bool
     )
+  end
+
+  def plus(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::PLUS,
+      as_expression,
+      expr.as_expression,
+      sql_type
+    )
+  end
+
+  def plus(value : T) : Stealth::BinaryExpression(T)
+    plus(wrap_argument(value))
+  end
+
+  def +(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    plus(expr)
+  end
+
+  def +(value : T) : Stealth::BinaryExpression(T)
+    plus(wrap_argument(value))
+  end
+
+  def minus(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::MINUS,
+      as_expression,
+      expr.as_expression,
+      sql_type
+    )
+  end
+
+  def minus(value : T) : Stealth::BinaryExpression(T)
+    minus(wrap_argument(value))
+  end
+
+  def -(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    minus(expr)
+  end
+
+  def -(value : T) : Stealth::BinaryExpression(T)
+    minus(wrap_argument(value))
+  end
+
+  def times(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::TIMES,
+      as_expression,
+      expr.as_expression,
+      sql_type
+    )
+  end
+
+  def times(value : T) : Stealth::BinaryExpression(T)
+    times(wrap_argument(value))
+  end
+
+  def *(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    times(expr)
+  end
+
+  def *(value : T) : Stealth::BinaryExpression(T)
+    times(wrap_argument(value))
+  end
+
+  def div(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::DIV,
+      as_expression,
+      expr.as_expression,
+      sql_type
+    )
+  end
+
+  def div(value : T) : Stealth::BinaryExpression(T)
+    div(wrap_argument(value))
+  end
+
+  def /(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    div(expr)
+  end
+
+  def /(value : T) : Stealth::BinaryExpression(T)
+    div(wrap_argument(value))
+  end
+
+  def rem(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::REM,
+      as_expression,
+      expr.as_expression,
+      sql_type
+    )
+  end
+
+  def rem(value : T) : Stealth::BinaryExpression(T)
+    rem(wrap_argument(value))
+  end
+
+  def %(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(T)
+    rem(expr)
+  end
+
+  def %(value : T) : Stealth::BinaryExpression(T)
+    rem(wrap_argument(value))
+  end
+
+  def like(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(B)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::LIKE,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def like(value : T) : Stealth::BinaryExpression(Bool)
+    like(wrap_argument(value))
+  end
+
+  def not_like(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::NOT_LIKE,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def not_like(value : T) : Stealth::BinaryExpression(Bool)
+    not_like(wrap_argument(value))
+  end
+
+  def and(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::AND,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def and(value : T) : Stealth::BinaryExpression(Bool)
+    and(wrap_argument(value))
+  end
+
+  def or(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::OR,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def or(value : T) : Stealth::BinaryExpression(Bool)
+    or(wrap_argument(value))
+  end
+
+  def xor(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::XOR,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def xor(value : T) : Stealth::BinaryExpression(Bool)
+    xor(wrap_argument(value))
+  end
+
+  def less_than(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::LESS_THAN,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def less_than(value : T) : Stealth::BinaryExpression(Bool)
+    less_than(wrap_argument(value))
+  end
+
+  def greater_than(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::GREATER_THAN,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def greater_than(value : T) : Stealth::BinaryExpression(Bool)
+    greater_than(wrap_argument(value))
+  end
+
+  def less_than_or_equal(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::LESS_THAN_OR_EQUAL,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def less_than_or_equal(value : T) : Stealth::BinaryExpression(Bool)
+    less_than_or_equal(wrap_argument(value))
+  end
+
+  def greater_than_or_equal(expr : ColumnDeclaring(T)) : Stealth::BinaryExpression(Bool)
+    Stealth::BinaryExpression.new(
+      BinaryExpressionType::GREATER_THAN_OR_EQUAL,
+      as_expression,
+      expr.as_expression,
+      Bool
+    )
+  end
+
+  def greater_than_or_equal(value : T) : Stealth::BinaryExpression(Bool)
+    greater_than_or_equal(wrap_argument(value))
   end
 end
