2b7f28ef19bffe92c2b605150386a804877ed5a6
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-11-12 13:45:57 -0600

    Change table implementation to use instances

--- src/stealth/table.cr
+++ src/stealth/table.cr
@@ -1,32 +1,44 @@
-module Stealth::Table
-  macro define(klass, table_name)
-    class {{ klass }}
-      COLUMNS = [] of Stealth::BaseColumn
-      {% verbatim do %}
-      private macro column(type_declaration)
-        {%
-          name = type_declaration.var
-          name_str = name.stringify
-          type = type_declaration.type
-        %}
-        COLUMNS << {{ name }}
-        class_getter {{ name }} : Stealth::Column({{ type }}) do
-          Stealth::Column.new(table: self, name: {{ name_str }}, sql_type: {{ type }})
-        end
-      end
-      {% end %}
+abstract class Stealth::Table
+  annotation ColumnLabel
+  end
 
-      extend Stealth::Table
+  macro define(klass, table_name)
+    class {{ klass }}_Table < Stealth::Table
 
       {{ yield }}
 
-      class_getter table_name : String = {{ table_name }}
-      class_getter columns : Array(Stealth::BaseColumn) = COLUMNS
+      @table_name = {{ table_name }}
+    end
+
+    {{ klass }} = {{ klass }}_Table.new
+  end
+
+  private macro column(type_declaration)
+    {%
+      name = type_declaration.var
+      name_str = name.stringify
+      type = type_declaration.type
+    %}
+
+    @[ColumnLabel]
+    getter {{ name }} : Stealth::Column({{ type }}) do
+      Stealth::Column.new(table: self, name: {{ name_str }}, sql_type: {{ type }})
     end
   end
 
-  abstract def table_name : String
-  abstract def columns : Array(Stealth::BaseColumn)
+  getter table_name : String
+  getter columns : Array(Stealth::BaseColumn) = [] of Stealth::BaseColumn
+
+  def initialize
+    {% begin %}
+      {% for ivar in @type.instance_vars %}
+        {% ann = ivar.annotation(::Stealth::Table::ColumnLabel) %}
+        {% if ann %}
+          columns << {{ ivar.id }}
+        {% end %}
+      {% end %}
+    {% end %}
+  end
 
   def as_expression : Stealth::TableExpression
     Stealth::TableExpression.new(name: table_name)
