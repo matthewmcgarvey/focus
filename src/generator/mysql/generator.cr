8f099c6abad24e2603cb2048b9715bf7c4184bb6
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2026-01-09 11:29:02 -0600

    Broken setup for mysql generator

---
+++ src/generator/mysql/generator.cr
@@ -0,0 +1,15 @@
+class Focus::MySqlGenerator
+  private getter db : Focus::DBConn
+  private getter dest_dir : String
+  private getter template : Focus::Template
+  private getter schema : String
+
+  def initialize(@db : Focus::DBConn, @dest_dir : String, @template : Focus::Template, @schema : String)
+  end
+
+  def generate
+    query_set = Focus::MySqlQuerySet.new(db)
+    schema_metadata = query_set.get_schema(schema)
+    template.process_schema(dest_dir, schema_metadata)
+  end
+end
