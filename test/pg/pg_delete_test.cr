6154cdd92b2a3f5d8f5ac6c790ad4abf7da653e2
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2026-01-10 00:07:54 -0600

    move over delete tests

--- test/pg/pg_delete_test.cr
+++ test/pg/pg_delete_test.cr
@@ -10,4 +10,13 @@ class PGDeleteTest < PGTestBase
       assert_nil Employees.select(Employees.id).where(Employees.id.eq(1)).query_one?(conn, Int32)
     end
   end
+
+  def test_delete_returning
+    in_transaction do |conn|
+      ids = Employees.delete.returning(Employees.id).query_all(conn, Int32)
+      assert ids.size > 0
+
+      assert_equal 0, Employees.select(Focus.count(Employees.id)).query_one(conn, Int64)
+    end
+  end
 end
