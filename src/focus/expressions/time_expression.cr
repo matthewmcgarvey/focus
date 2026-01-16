8ebbd6dad959c6421b3806cef9024898221c5587
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2026-01-15 23:56:42 -0600

    TimeExpression functions

--- src/focus/expressions/time_expression.cr
+++ src/focus/expressions/time_expression.cr
@@ -7,4 +7,40 @@ class Focus::TimeExpression < Focus::Expression
   def eq(rhs : Focus::TimeExpression) : Focus::BoolExpression
     _eq(rhs)
   end
+
+  def not_eq(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _not_eq(rhs)
+  end
+
+  def is_distinct_from(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _is_distinct_from(rhs)
+  end
+
+  def is_not_distinct_from(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _is_not_distinct_from(rhs)
+  end
+
+  def less_than(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _less_than(rhs)
+  end
+
+  def less_than_eq(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _less_than_eq(rhs)
+  end
+
+  def greater_than(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _greater_than(rhs)
+  end
+
+  def greater_than_eq(rhs : Focus::TimeExpression) : Focus::BoolExpression
+    _greater_than_eq(rhs)
+  end
+
+  def between(min : Focus::TimeExpression, max : Focus::TimeExpression) : Focus::BoolExpression
+    _between(min, max, negated: false)
+  end
+
+  def not_between(min : Focus::TimeExpression, max : Focus::TimeExpression) : Focus::BoolExpression
+    _between(min, max, negated: true)
+  end
 end
