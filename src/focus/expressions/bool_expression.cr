6fd24e0ad6b1b52c67b11eb0aa1764de35114070
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2026-01-15 21:02:03 -0600

    BoolExpression functions

--- src/focus/expressions/bool_expression.cr
+++ src/focus/expressions/bool_expression.cr
@@ -4,17 +4,57 @@ class Focus::BoolExpression < Focus::Expression
   def initialize(@inner : Focus::Expression? = nil)
   end
 
-  def and(right : Focus::BoolExpression) : Focus::BoolExpression
-    binary_expr = Focus::BinaryExpression.new(self, right, "AND")
-    Focus::BoolExpression.new(binary_expr)
+  def and(rhs : Focus::BoolExpression) : Focus::BoolExpression
+    binary_op("AND", rhs)
   end
 
-  def or(right : Focus::BoolExpression) : Focus::BoolExpression
-    binary_expr = Focus::BinaryExpression.new(self, right, "OR")
-    Focus::BoolExpression.new(binary_expr)
+  def or(rhs : Focus::BoolExpression) : Focus::BoolExpression
+    binary_op("OR", rhs)
   end
 
   def eq(rhs : Focus::BoolExpression) : Focus::BoolExpression
     _eq(rhs)
+  end
+
+  def not_eq(rhs : Focus::BoolExpression) : Focus::BoolExpression
+    _not_eq(rhs)
+  end
+
+  def is_distinct_from(rhs : Focus::BoolExpression) : Focus::BoolExpression
+    _is_distinct_from(rhs)
+  end
+
+  def is_not_distinct_from(rhs : Focus::BoolExpression) : Focus::BoolExpression
+    _is_not_distinct_from(rhs)
+  end
+
+  def is_true : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS TRUE")
+    Focus::BoolExpression.new(expression)
+  end
+
+  def is_not_true : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS NOT TRUE")
+    Focus::BoolExpression.new(expression)
+  end
+
+  def is_false : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS FALSE")
+    Focus::BoolExpression.new(expression)
+  end
+
+  def is_not_false : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS NOT FALSE")
+    Focus::BoolExpression.new(expression)
+  end
+
+  def is_unknown : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS UNKNOWN")
+    Focus::BoolExpression.new(expression)
+  end
+
+  def is_not_unknown : Focus::BoolExpression
+    expression = Focus::PostfixOperatorExpression.new(self, "IS NOT UNKNOWN")
+    Focus::BoolExpression.new(expression)
   end
 end
