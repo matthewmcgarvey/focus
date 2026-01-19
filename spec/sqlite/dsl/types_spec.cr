e6fda298bcabb2746136ab5526eedf271f44b128
Matthew <matthewmcgarvey14@gmail.com>
2026-01-18 22:37:39 -0600

    Added sqlite DATETIME literal function

--- spec/sqlite/dsl/types_spec.cr
+++ spec/sqlite/dsl/types_spec.cr
@@ -22,54 +22,33 @@ describe Focus::SQLite::Dsl::Types do
     end
   end
 
-  # describe ".timestamp" do
-  #   it "supports separate args" do
-  #     timestamp = Focus::SQLite.timestamp(2026, 1, 17, hour: 10, minute: 25, second: 30)
+  describe ".datetime" do
+    it "supports separate args" do
+      datetime = Focus::SQLite.datetime(2026, 1, 17, hour: 10, minute: 25, second: 30)
 
-  #     visitor = Focus::SQLiteFormatter.new
-  #     timestamp.accept(visitor)
-  #     visitor.to_sql.should eq("CAST(? AS TIMESTAMP)")
-  #     visitor.parameters.should eq(["2026-01-17 10:25:30"])
-  #   end
+      visitor = Focus::SQLiteFormatter.new
+      datetime.accept(visitor)
+      visitor.to_sql.should eq("DATETIME(?)")
+      visitor.parameters.should eq(["2026-01-17 10:25:30"])
+    end
 
-  #   it "supports separate args with nanoseconds" do
-  #     timestamp = Focus::SQLite.timestamp(2026, 1, 17, hour: 10, minute: 25, second: 30, nanoseconds: 100)
+    it "supports separate args with nanoseconds" do
+      datetime = Focus::SQLite.datetime(2026, 1, 17, hour: 10, minute: 25, second: 30, nanoseconds: 100)
 
-  #     visitor = Focus::SQLiteFormatter.new
-  #     timestamp.accept(visitor)
-  #     visitor.to_sql.should eq("CAST(? AS TIMESTAMP)")
-  #     visitor.parameters.should eq(["2026-01-17 10:25:30.0000001"])
-  #   end
+      visitor = Focus::SQLiteFormatter.new
+      datetime.accept(visitor)
+      visitor.to_sql.should eq("DATETIME(?)")
+      visitor.parameters.should eq(["2026-01-17 10:25:30.0000001"])
+    end
 
-  #   it "supports time" do
-  #     time = Time.utc
-  #     timestamp = Focus::SQLite.timestamp(time)
+    it "supports time" do
+      time = Time.utc
+      timestamp = Focus::SQLite.datetime(time)
 
-  #     visitor = Focus::SQLiteFormatter.new
-  #     timestamp.accept(visitor)
-  #     visitor.to_sql.should eq("CAST(? AS TIMESTAMP)")
-  #     visitor.parameters.should eq([time])
-  #   end
-  # end
-
-  # describe ".timestamp_tz" do
-  #   it "supports separate args" do
-  #     timestamp_tz = Focus::SQLite.timestamp_tz(2026, 1, 17, hour: 10, minute: 25, second: 30, nanoseconds: 0, time_zone: "GMT")
-
-  #     visitor = Focus::SQLiteFormatter.new
-  #     timestamp_tz.accept(visitor)
-  #     visitor.to_sql.should eq("CAST(? AS TIMESTAMPTZ)")
-  #     visitor.parameters.should eq(["2026-01-17 10:25:30 GMT"])
-  #   end
-
-  #   it "supports time" do
-  #     time = Time.utc
-  #     timestamp_tz = Focus::SQLite.timestamp_tz(time)
-
-  #     visitor = Focus::SQLiteFormatter.new
-  #     timestamp_tz.accept(visitor)
-  #     visitor.to_sql.should eq("CAST(? AS TIMESTAMPTZ)")
-  #     visitor.parameters.should eq([time])
-  #   end
-  # end
+      visitor = Focus::SQLiteFormatter.new
+      timestamp.accept(visitor)
+      visitor.to_sql.should eq("DATETIME(?)")
+      visitor.parameters.should eq([time])
+    end
+  end
 end
--- src/sqlite/dsl/types.cr
+++ src/sqlite/dsl/types.cr
@@ -12,35 +12,19 @@ module Focus::SQLite::Dsl::Types
     Focus::DateExpression.new(func)
   end
 
-  # TODO
-  # def timestamp(year : Int32, month : Int8, day : Int8, hour : Int8, minute : Int8, second : Int8, nanoseconds : Int32? = nil) : Focus::TimestampExpression
-  #   time_str = sprintf("%04d-%02d-%02d %02d:%02d:%02d", {year, month, day, hour, minute, second})
-  #   time_str += format_nanoseconds(nanoseconds) if nanoseconds
-  #   literal = Focus::LiteralExpression.new(time_str)
-  #   timestamp_expr = Focus::TimestampExpression.new(literal)
-  #   Focus::SQLite.cast(timestamp_expr).as_timestamp
-  # end
+  def datetime(year : Int32, month : Int8, day : Int8, hour : Int8, minute : Int8, second : Int8, nanoseconds : Int32? = nil) : Focus::TimestampExpression
+    time_str = sprintf("%04d-%02d-%02d %02d:%02d:%02d", {year, month, day, hour, minute, second})
+    time_str += format_nanoseconds(nanoseconds) if nanoseconds
+    literal = Focus::LiteralExpression.new(time_str)
+    func = Focus::FunctionExpression.new("DATETIME", [literal] of Focus::Expression)
+    Focus::TimestampExpression.new(func)
+  end
 
-  # def timestamp(time : Time) : Focus::TimestampExpression
-  #   literal = Focus::LiteralExpression.new(time)
-  #   timestamp_expr = Focus::TimestampExpression.new(literal)
-  #   Focus::SQLite.cast(timestamp_expr).as_timestamp
-  # end
-
-  # def timestamp_tz(year : Int32, month : Int8, day : Int8, hour : Int8, minute : Int8, second : Int8, nanoseconds : Int32, time_zone : String) : Focus::TimestampTzExpression
-  #   time_str = sprintf("%04d-%02d-%02d %02d:%02d:%02d", {year, month, day, hour, minute, second})
-  #   time_str += format_nanoseconds(nanoseconds)
-  #   time_str += ' ' + time_zone
-  #   literal = Focus::LiteralExpression.new(time_str)
-  #   timestamp_expr = Focus::TimestampTzExpression.new(literal)
-  #   Focus::SQLite.cast(timestamp_expr).as_timestamp_tz
-  # end
-
-  # def timestamp_tz(time : Time) : Focus::TimestampTzExpression
-  #   literal = Focus::LiteralExpression.new(time)
-  #   timestamp_expr = Focus::TimestampTzExpression.new(literal)
-  #   Focus::SQLite.cast(timestamp_expr).as_timestamp_tz
-  # end
+  def datetime(time : Time) : Focus::TimestampExpression
+    literal = Focus::LiteralExpression.new(time)
+    func = Focus::FunctionExpression.new("DATETIME", [literal] of Focus::Expression)
+    Focus::TimestampExpression.new(func)
+  end
 
   # max is 999
   # value like 300 will have trailing '0's removed leaving only '3'
